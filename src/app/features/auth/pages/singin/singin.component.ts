6f46eb60b6d9cecdf969cd1e6546c52db93fe529
ahmed yasser <ahmedya3ser8@gmail.com>
2026-05-07 22:25:48 +0300

    feat: add auth-layout and singup, singin pages ui

--- src/app/features/auth/pages/singin/singin.component.ts
+++ src/app/features/auth/pages/singin/singin.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { NgIcon } from '@ng-icons/core';
 
 @Component({
   selector: 'app-singin',
-  imports: [],
+  imports: [NgIcon, RouterLink],
   templateUrl: './singin.component.html',
   styleUrl: './singin.component.scss',
 })
--- src/app/features/auth/pages/singup/singup.component.ts
+++ src/app/features/auth/pages/singup/singup.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { NgIcon } from '@ng-icons/core';
 
 @Component({
   selector: 'app-singup',
-  imports: [],
+  imports: [NgIcon, RouterLink],
   templateUrl: './singup.component.html',
   styleUrl: './singup.component.scss',
 })
